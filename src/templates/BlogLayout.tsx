594e64ff1122f88daa997a94886a65f63434bff9
okwasniewski <oskarkwasniewski@icloud.com>
2021-08-09 23:18:07 +0200

    update padding on mobile:

--- src/templates/BlogLayout.tsx
+++ src/templates/BlogLayout.tsx
@@ -15,8 +15,8 @@ const BlogLayout = ({
 }: BlogLayoutProps) => {
   return (
     <MainTemplate title={title}>
-      <div className="bg-gray-50 full-width">
-        <div className="container pb-10 mx-auto mb-8 pt-14">
+      <div className='p-4 md:p-0 bg-gray-50 full-width'>
+        <div className='container pb-10 mx-auto mb-8 pt-14'>
           <h1 className='mb-1 text-3xl font-semibold '>{title}</h1>
           <p className='mb-4'>{subtitle}</p>
         </div>
