8d5ea4c047e0cf8fca650a40069206b6840eba26
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2022-02-06 18:12:03 +0100

    Fix Heading font sizes

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 interface HeadingProps {
   heading: string;
@@ -11,10 +11,10 @@ const Heading = ({ heading, subHeading, white = false }: HeadingProps) => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 1 } }}
-      className={`mb-4 ${white ? 'text-white' : 'text-gray-900'}`}
+      className={`mb-4 ${white ? "text-white" : "text-gray-900"}`}
     >
-      <h2 className='text-2xl font-bold'>{heading}</h2>
-      <p>{subHeading}</p>
+      <h2 className='text-3xl font-bold'>{heading}</h2>
+      <p className='text-md'>{subHeading}</p>
     </motion.div>
   );
 };
