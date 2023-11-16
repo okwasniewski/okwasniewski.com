0ba396419648eb3b0375acdb2b25a05a49c41ffe
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2023-11-16 14:39:26 +0100

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/style.css'
+import "../styles/style.css";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
