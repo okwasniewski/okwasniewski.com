4e1439002522d722a0583ce1c6bc2c8ff132ea2a
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2023-11-20 21:55:40 +0100

    feat: add additional twitter tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,11 +9,13 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
+        <meta property="og:locale" content="en_US" />
         <meta
           property="og:site_name"
           content="Oskar Kwaśniewski"
           key="ogsitename"
         />
+        <meta name="author" content="Oskar Kwaśniewski" />
         <meta property="og:url" content="oskarkwasniewski.dev" key="ogurl" />
         <meta
           property="og:image"
@@ -22,6 +24,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@o_kwasniewski" />
+        <meta name="twitter:site" content="@o_kwasniewski" />
       </Head>
       <Component {...pageProps} />
       <Analytics />
