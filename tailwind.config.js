9f60f8bcd40ee63c6ce699cdd89b9b8a4a683d52
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-01-07 18:39:27 +0100

    feat: enchance syntax highlighting

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,29 @@ module.exports = {
       "text-blue-500",
       "bg-green-200",
       "text-green-500",
+      "bg-orange-200",
+      "text-orange-500",
     ],
   },
   theme: {
     extend: {
-      colors: { 
+      colors: {
         "dark-surface": "#1E1D1D",
-        "dark-surface-accent": "#2D2C2C"
+        "dark-surface-accent": "#2D2C2C",
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            pre: {
+              background: "rgba(205, 200, 255, 0.05)"
+            },
+            code: {
+              '&::before': { content: `unset !important` },
+              '&::after': { content: `unset !important` },
+              fontWeight: 'normal',
+            },
+          },
+        },
       },
     },
   },
